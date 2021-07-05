1fc0aff0f84c7fc4ec4d7c4ec8a98c2a8ff83459
programsec-bud <83387318+programsec-bud@users.noreply.github.com>
2021-07-05 14:20:18 +0900

    Create K번째수.py

---
+++ K번째수.py
@@ -0,0 +1,13 @@
+"""
+-프로그래머스 코딩테스트 문제:"K번째 수"
+"""
+def solution(array, commands):
+    #return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
+    answer = []
+
+    for command in commands:
+        i,j,k = command
+        
+        answer.append(sorted(array[i-1:j])[k-1])
+        
+    return answer
