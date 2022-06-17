9da1662b1401e5392a51d86d7d892595e7106f76
programsec-bud <83387318+programsec-bud@users.noreply.github.com>
2022-06-17 09:31:00 +0900

    Create 11651.py

---
+++ 백준알고리즘/11651.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+lst = list()
+for _ in range(n):
+    x,y = map(int,sys.stdin.readline().split())
+
+    lst.append((x,y))
+lst.sort(key=lambda x: x[0])
+lst.sort(key=lambda x: x[1])
+
+for a,b in lst:
+    print(a,b)
