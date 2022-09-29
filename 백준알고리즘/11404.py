3d4e8db488a1b095fd5e71302caa86846a82b815
ljh98 <83387318+BewineLog@users.noreply.github.com>
2022-09-29 10:18:28 +0900

    Create 11404.py

---
+++ 백준알고리즘/11404.py
@@ -0,0 +1,34 @@
+import sys
+
+INF = sys.maxsize
+
+n = int(input())    # number of city
+m = int(input())    # number of bus
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph[a][b] = min(graph[a][b],c)
+
+# for i in range(1,n+1):
+#     print(graph[i][1:])
+
+# D_ab = D_ak + D_kb
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for i in range(1,n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            graph[i][j] = 0
+for i in range(1, n+1):
+    print(*graph[i][1:])
+    # print(' '.join(map(str,graph[i][1:])))
