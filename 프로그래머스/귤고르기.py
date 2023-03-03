11e4da85335385eca82ddc1bbcec7c8d05623b64
ljh98 <83387318+BewineLog@users.noreply.github.com>
2023-03-03 15:31:53 +0900

    Update 귤고르기.py

--- 프로그래머스/귤고르기.py
+++ 프로그래머스/귤고르기.py
@@ -2,9 +2,6 @@ from collections import Counter
 def solution(k, tangerine):
     answer = 0
     dic = Counter(tangerine)
-    # print(dic)
-    # for i in tangerine:
-    #     dic[str(i)] = 1 if str(i) not in dic.keys() else dic[str(i)] + 1
     
     for i in dic.most_common():
         answer += 1
